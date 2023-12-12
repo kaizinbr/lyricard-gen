418a4c29231252c409deacac0d2600e394405efd
Kaizin <kaiolucas1812@gmail.com>
2023-12-11 22:21:58 -0300

    como faz deploy jss

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
 // export const metadata = {
 //   title: 'Vercel Blob Starter',
 //   description: 'A simple Next.js app with Vercel Blob for image uploads',
